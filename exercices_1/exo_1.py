b4dc4f7c6b8fbca23bd2bf6aa30264035fc7fc3a
Lepython <passywebmaster@gmail.com>
2019-05-28 09:27:45 +0100

    modification de l'exercice 1

--- exercices_1/exo_1.py
+++ exercices_1/exo_1.py
@@ -1,4 +1,3 @@
-
 def read_csv (mon_fichier, num_lines = 31):
     with open(mon_fichier, 'r') as f:
         lines = f.readlines()
@@ -7,7 +6,7 @@ def read_csv (mon_fichier, num_lines = 31):
             line = line.split("," )
             line =  " {}, est le n° : {}, il s'agit d'un {}, ".format(line[1], line[0], line[4]) 
             print(line)
-    return line
+        return line
             
 def write_csv (mon_fichier, my_list):
     with open(mon_fichier, 'w') as f :
@@ -15,11 +14,4 @@ def write_csv (mon_fichier, my_list):
 
 
 f = read_csv("mock_data.csv")
-write_csv("mock_data_1.csv", f )
- 
- 
-
-
-
-
-
+write_csv("mock_data_1.csv", f ) 

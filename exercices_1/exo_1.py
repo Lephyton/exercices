8388e49cc4737798c11bbb14009c5842544549c2
Lepython <passywebmaster@gmail.com>
2019-05-28 03:55:47 +0100

    modification du code de exo_1

--- exercices_1/exo_1.py
+++ exercices_1/exo_1.py
@@ -7,7 +7,8 @@ def read_csv (mon_fichier, num_lines = 31):
             line = line.split("," )
             line =  " {}, est le n° : {}, il s'agit d'un {}, ".format(line[1], line[0], line[4]) 
             print(line)
-        return line
+    return line
+            
 def write_csv (mon_fichier, my_list):
     with open(mon_fichier, 'w') as f :
          f.writelines(my_list)
@@ -15,7 +16,7 @@ def write_csv (mon_fichier, my_list):
 
 f = read_csv("mock_data.csv")
 write_csv("mock_data_1.csv", f )
-
+ 
  
 
 

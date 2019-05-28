ebaeab81191304e8e7d6c136c88470f14622622a
Lepython <passywebmaster@gmail.com>
2019-05-28 10:01:15 +0100

    supression des fichier suplementatire du dossier

--- exercices_1/game.py
+++
@@ -1,9 +0,0 @@
-num_real = 122
-while num_real in range(1, 1000):
-    number = int((input("Entrer un nobre")) )
-    if number == num_real:
-        print("c'est bon et fin du jeux")
-    elif number < num_real :
-        print("c'est trop petit")
-    elif number > num_real:
-        print("c'est trop grand")
--- exercices_1/is.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-
-Contact_df = pd.read_csv(mock_data.csv)
-
-Contact_df['format'] = contact_df[['first_name', 'id']].apply(lambda x: f '{x[0]}, est le numero : {x[1]}')
-
-
-
-

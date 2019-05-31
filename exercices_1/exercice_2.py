8a22afbc01560cb312870c7b6ba5c336238f7353
Lepython <passywebmaster@gmail.com>
2019-05-31 19:24:41 +0100

    modification des fichier

--- exercices_1/exercice_2.py
+++ exercices_1/exercice_2.py
@@ -1,16 +1,25 @@
 #ouvrir le fichier moock_data le lire 
 #recuperer toutes les lignes dont la colone gender est egal à male
 #creer  un autre fichier csv et y ecrire ces informations
-
-def read_csv(mon_fichier):
-    with open(mon_fichier, 'r') as f:
-        lines = f.DictReader()
-        for line in lines :
-            line = line.split(",")
-            print(line)
-
-read_csv("mock_data.csv")
-       
-
-
  
+
+def read_csv (mon_fichier):
+    with open(mon_fichier, 'r') as f:
+        lines = f.readlines()
+        result = []
+        for line in lines :     
+            line = line.split("," ) 
+            if line[4] == "Male" :
+                line = ','.join(line)
+                result.append(line) 
+                print(line)
+
+        return result
+            
+def write_csv (mon_fichier, my_list):
+     with open(mon_fichier, 'w', encoding = "utf8") as f :
+         f.writelines(my_list)
+
+
+f = read_csv("mock_data.csv")
+write_csv("mock_data_male.csv", f) 
--- exercices_1/exo_1.py
+++ exercices_1/exo_1.py
@@ -1,17 +1,20 @@
+
 def read_csv (mon_fichier, num_lines = 31):
     with open(mon_fichier, 'r') as f:
         lines = f.readlines()
-
+        lines = lines[:num_lines]
+        result = []
         for line in lines :
             line = line.split("," )
             line =  " {}, est le n° : {}, il s'agit d'un {}, ".format(line[1], line[0], line[4]) 
+            result.append(line + "\n")
             print(line)
-        return line
+        return result
             
 def write_csv (mon_fichier, my_list):
-    with open(mon_fichier, 'w') as f :
+    with open(mon_fichier, 'w', encoding = "utf8") as f :
          f.writelines(my_list)
 
 
 f = read_csv("mock_data.csv")
-write_csv("mock_data_1.csv", f ) 
+write_csv("mock_data_1.csv", f) 
